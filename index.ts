2f2cc782d78240d0f5add4ef76d688465ef58fd7
Uros Cirkovic <cirkovic.uros.srb@gmail.com>
2019-09-14 00:33:45 +0200

    Add tslint dependencies - to try to enable auto formatting

---
+++ index.ts
@@ -0,0 +1,13 @@
+export default class DummyClass {
+  public msg: string
+  public   age: number
+  
+  
+
+  constructor()   {
+                                  const onevar = 'sdfsd';
+     const anothervar   = 'asldfjiowwer';
+
+
+  }
+}
